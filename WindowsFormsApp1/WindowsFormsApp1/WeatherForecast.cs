9d3a79629bd7c632c0182ab478675cc90761c345
Andreea Stati <andreea.stati@student.tuiasi.ro>
2025-05-01 22:56:31 +0300

    aplicatie meteo utilizand tutorial yt

---
+++ WindowsFormsApp1/WindowsFormsApp1/WeatherForecast.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class WeatherForecast
+    {
+        public class main //temp
+        {
+            public double temp_min { get; set; }
+            public double temp_max { get; set; }
+
+        }
+
+        public class weather
+        {
+            public string main { get; set; }
+            public string description { get; set; }
+            public string icon { get; set; }
+        }
+
+        public class list
+        {
+            public long dt { get; set; } // day time
+            public main main { get; set; }
+            public List<weather> weather { get; set; }
+        }
+
+        public class ForecastInfo
+        {
+            public List<list> list { get; set; }
+        }
+    }
+}
