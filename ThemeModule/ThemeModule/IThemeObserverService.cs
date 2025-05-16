c82519dde4f08498d4788c4bb474283539bb585b
anton1n <iva.antonin1999@gmail.com>
2025-05-16 13:27:05 +0300

    restructurare DLL-uri si schimbare la .NET 8

---
+++ ThemeModule/ThemeModule/IThemeObserverService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThemeModule
+{
+    public enum AppTheme
+    {
+        Light,
+        Dark
+    }
+    public interface IThemeObserverService
+    {
+        /// <summary>
+        /// Metodă pentru schimbarea tematicii grafice a aplicației.
+        /// </summary>
+        /// <param name="appTheme"></param>
+        public void OnThemeChanged(AppTheme appTheme);
+    }
+
+}
