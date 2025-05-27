f74e9703405b26039d3c37051b3e7558cea4ac3e
Andreea Stati <andreea.stati@student.tuiasi.ro>
2025-05-27 22:05:32 +0300

    imagini pt readme

--- WeatherApp/WeatherApp/obj/Debug/net8.0-windows7.0/WeatherApp.AssemblyInfo.cs
+++ WeatherApp/WeatherApp/obj/Debug/net8.0-windows7.0/WeatherApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14e88cfc898eeb94f41c3291647c3971fbd74f28")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2d1a2e074639a74e298a398c393f5dbfb54b21d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
